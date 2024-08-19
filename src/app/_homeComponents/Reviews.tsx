596ec6e84e22788862908882f2b120ecf9307518
Ashar <asharmeraj55@gmail.com>
2024-08-19 20:32:57 +0500

    Homepage added

--- src/app/_homeComponents/Reviews.tsx
+++ src/app/_homeComponents/Reviews.tsx
@@ -9,7 +9,7 @@ const Reviews = () => {
                         <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
                     </svg>
                     <blockquote>
-                        <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">"AIM is just awesome. The AI interview is highly beneficial for learning and for self-assessment.AIM is the perfect choice for your next interview."</p>
+                        <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">&#34;AIM is just awesome. The AI interview is highly beneficial for learning and for self-assessment.AIM is the perfect choice for your next interview.&#34;</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center mt-6 space-x-3">
                         <img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture" />
